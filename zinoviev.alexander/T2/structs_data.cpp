48796cb997869f0c4dbdea87291e6edd8de435ce
FShaaM <zinovevas135@gmail.com>
2026-05-18 23:10:16 +0300

    Fixed CODE 5

--- zinoviev.alexander/T2/structs_data.cpp
+++ zinoviev.alexander/T2/structs_data.cpp
@@ -74,3 +74,12 @@ std::ostream& zinoviev::operator<<(std::ostream& out, const DataStruct& src)
 
   return out;
 }
+
+bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  if (lhs.key1 != rhs.key1)
+    return lhs.key1 < rhs.key1;
+  if (lhs.key2 != rhs.key2)
+    return lhs.key2 < rhs.key2;
+  return lhs.key3.size() < rhs.key3.size();
+}
