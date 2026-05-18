2a6991df0eab0af446dbfe46607b89b602793fdc
FShaaM <zinovevas135@gmail.com>
2026-05-18 23:17:05 +0300

    Fixed CODE 6

--- zinoviev.alexander/T2/structs_data.cpp
+++ zinoviev.alexander/T2/structs_data.cpp
@@ -2,84 +2,89 @@
 #include "structs_ios.hpp"
 #include <iomanip>
 
-std::istream& zinoviev::operator>>(std::istream& in, DataStruct& dest)
+namespace zinoviev
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
-    return in;
-
-  DataStruct input;
-  bool hasKey1 = false;
-  bool hasKey2 = false;
-  bool hasKey3 = false;
-
-  in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
-  while (in)
+  std::istream& operator>>(std::istream& in, DataStruct& dest)
   {
-    in >> std::ws;
-
-    if (in.peek() == ')')
-    {
-      in >> DelimiterIO{ ')' };
-      break;
-    }
-
-    std::string name;
-    in >> name;
-
-    if (name == "key1")
-    {
-      in >> DoubleLitIO{ input.key1 };
-      hasKey1 = true;
-    }
-    else if (name == "key2")
-    {
-      in >> UllLitIO{ input.key2 };
-      hasKey2 = true;
-    }
-    else if (name == "key3")
-    {
-      in >> StringIO{ input.key3 };
-      hasKey3 = true;
-    }
-    else
-    {
-      in.setstate(std::ios::failbit);
+    std::istream::sentry sentry(in);
+    if (!sentry)
       return in;
+
+    DataStruct input;
+    bool hasKey1 = false;
+    bool hasKey2 = false;
+    bool hasKey3 = false;
+
+    in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
+    while (in)
+    {
+      in >> std::ws;
+
+      if (in.peek() == ')')
+      {
+        in >> DelimiterIO{ ')' };
+        break;
+      }
+
+      std::string name;
+      in >> name;
+
+      if (name == "key1")
+      {
+        in >> DoubleLitIO{ input.key1 };
+        hasKey1 = true;
+      }
+      else if (name == "key2")
+      {
+        in >> UllLitIO{ input.key2 };
+        hasKey2 = true;
+      }
+      else if (name == "key3")
+      {
+        in >> StringIO{ input.key3 };
+        hasKey3 = true;
+      }
+      else
+      {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+
+      in >> DelimiterIO{ ':' };
+      if (!in)
+        return in;
     }
 
-    in >> DelimiterIO{ ':' };
+    if (hasKey1 && hasKey2 && hasKey3)
+      dest = input;
+    else
+      in.setstate(std::ios::failbit);
+
+    return in;
   }
 
-  if (hasKey1 && hasKey2 && hasKey3)
-    dest = input;
-  else
-    in.setstate(std::ios::failbit);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+      return out;
 
-  return in;
-}
+    IOGuard guard(out);
 
-std::ostream& zinoviev::operator<<(std::ostream& out, const DataStruct& src)
-{
-  std::ostream::sentry sentry(out);
-  if (!sentry)
+    out << "(:key1 "
+        << std::fixed << std::setprecision(1) << src.key1 << "d"
+        << ":key2 " << src.key2 << "ull"
+        << ":key3 \"" << src.key3 << "\":)";
+
     return out;
+  }
 
-  IOGuard guard(out);
-
-  out << "(:key1 "
-    << std::fixed << std::setprecision(1) << src.key1 << "d"
-    << ":key2 " << src.key2 << "ull"
-    << ":key3 \"" << src.key3 << "\":)";
-
-  return out;
-}
-
-bool operator<(const DataStruct& lhs, const DataStruct& rhs)
-{
-  if (lhs.key1 != rhs.key1)
-    return lhs.key1 < rhs.key1;
-  if (lhs.key2 != rhs.key2)
-    return lhs.key2 < rhs.key2;
-  return lhs.key3.size() < rhs.key3.size();
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+  {
+    if (lhs.key1 != rhs.key1)
+      return lhs.key1 < rhs.key1;
+    if (lhs.key2 != rhs.key2)
+      return lhs.key2 < rhs.key2;
+    return lhs.key3.size() < rhs.key3.size();
+  }
 }
