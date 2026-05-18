ffcba5909954398696077840b71237eaaf3de353
FShaaM <zinovevas135@gmail.com>
2026-05-18 22:06:14 +0300

    Added main()

--- zinoviev.alexander/T2/main.cpp
+++ zinoviev.alexander/T2/main.cpp
@@ -1,2 +1,31 @@
+#include "structs_ios.hpp"
+#include "structs_data.hpp"
+
 int main()
-{}
+{
+  using namespace zinoviev;
+  std::vector<DataStruct> data;
+
+  std::copy(
+    std::istream_iterator<DataStruct>{std::cin},
+    std::istream_iterator<DataStruct>{},
+    std::back_inserter(data)
+  );
+
+  std::sort(data.begin(), data.end(),
+    [](const DataStruct& a, const DataStruct& b)
+    {
+      if (a.key1 != b.key1)
+        return a.key1 < b.key1;
+      if (a.key2 != b.key2)
+        return a.key2 < b.key2;
+      return a.key3.size() < b.key3.size();
+    }
+  );
+
+  std::copy(
+    data.begin(),
+    data.end(),
+    std::ostream_iterator<DataStruct>(std::cout, "\n")
+  );
+}
