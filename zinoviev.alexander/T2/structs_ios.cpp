1d5a577e9fd80ed030e186c82038f864e76188dd
FShaaM <zinovevas135@gmail.com>
2026-05-18 22:18:04 +0300

    Fixed code 2

--- zinoviev.alexander/T2/structs_ios.cpp
+++ zinoviev.alexander/T2/structs_ios.cpp
@@ -80,12 +80,12 @@ std::istream& zinoviev::operator>>(std::istream& in, const LabelIO&& dest)
   return in;
 }
 
-zinoviev::IOguard::IOguard(std::basic_ios<char>& s)
-  : s_(s),
+zinoviev::IOguard::IOguard(std::basic_ios<char>& s) :
+  s_(s),
   width_(s.width()),
-  fill_(s.fill()),
   precision_(s.precision()),
-  fmt_(s.flags())
+  fmt_(s.flags()),
+  fill_(s.fill())
 {
 }
 
